6528adc42d6d44e99fab0b19decff3792f344a73
Harshikesh <harshibabu604@gmail.com>
2024-08-19 14:13:57 +0530

    onClick and useState used.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
 
-const todoList =[{rowNo:1, description:"water plants", assignedTo:"User 1"},
-  {rowNo:2, description:"take dog out", assignedTo:"User 2"},
-  {rowNo:3, description:"get haircut", assignedTo:"User 1"},
-  {rowNo:34, description:"shake bilbul", assignedTo:"User 3"}
-];
+
+
 
 function App() {
+ const [todoList, setTodoList] = useState( [
+  {rowNo:1, description:"water plants", assignedTo:"User 1"},
+  {rowNo:2, description:"take dog out", assignedTo:"User 2"},
+  {rowNo:3, description:"get haircut", assignedTo:"User 1"},
+  {rowNo:4, description:"shake bilbul", assignedTo:"User 3"}
+]);
+  
+  const addTodo =() =>{
+    if(todoList.length>=1){
+      const newTodoRow ={
+        rowNo: todoList.length+1,
+        description: "new description",
+        assignedTo:"new user"
+      };
+      setTodoList(todoList =>[...todoList, newTodoRow]);
+    }
+  }
+
   return (
     <div className="mt-5 container">
       <div className="card">
@@ -16,6 +32,7 @@ function App() {
       </div>
       <div className="card-body">
        <TodoTable todoList = {todoList}/>
+       <button className="btn btn-primary" onClick={addTodo}>Add new todo</button>
       </div>
       </div>
     </div>
